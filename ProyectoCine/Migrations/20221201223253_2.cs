b1bd7e26351fd9b2c653d6b9529431faa69a1438
guidonaselli <89815219+guidonaselli@users.noreply.github.com>
2022-12-01 23:03:33 -0300

    Frontend de las entradas, diseño del footer y modelo final detalle de peliculas

---
+++ ProyectoCine/Migrations/20221201223253_2.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoCine.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Fila",
+                table: "entradas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Horario",
+                table: "entradas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombrePelicula",
+                table: "entradas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombreYApellido",
+                table: "entradas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Horario",
+                table: "entradas");
+
+            migrationBuilder.DropColumn(
+                name: "NombrePelicula",
+                table: "entradas");
+
+            migrationBuilder.DropColumn(
+                name: "NombreYApellido",
+                table: "entradas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Fila",
+                table: "entradas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
